1b0913ae24c3a0dbb7d4051965798a71ec24b4e1
CrimsonCoder42 <nostro37@gmail.com>
2023-03-07 10:27:59 -0500

    updated

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,23 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/**");
+
+  eleventyConfig.addShortcode("laureateDisplayName",
+      function(laureate) {
+        let displayName = '';
+        if (("knownName" in laureate)
+            && ("en" in laureate.knownName)) {
+          displayName = laureate.knownName.en;
+        } else if (("orgName" in laureate)
+            && ("en" in laureate.orgName)) {
+          displayName = laureate.orgName.en;
+        }
+        return displayName;
+      });
+
   return {
-    dir: {
-      input: "src",
-      output: "dist"
+    "dir" : {
+      "input": "src",
+      "output": "dist"
     }
   };
-}
+};
--- src/index.md
+++ src/index.md
@@ -1,1 +1,8 @@
-# Hello There 
+---
+layout: base
+title : Hello Class!
+---
+
+# Hello CSCI E-114
+
+This is a test of the emergency broadcast system. This is only a test.
